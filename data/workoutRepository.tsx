a6136683ac3eb123e274d0773e77b139f43a4245
Peter Mullins <jc310xc@gmail.com>
2024-03-04 17:39:58 -0500

    wip

--- data/workoutRepository.tsx
+++ data/workoutRepository.tsx
@@ -6,9 +6,12 @@ const db = SQLite.openDatabase("workoutNotes.db");
 const migrations = 
 [
     `create table if not exists workouts (id integer primary key, date text);
-    create table if not exists exercise_types (id integer primary key, exercise_name text);
+    create table if not exists exercise_types (id integer primary key, exercise_name text not null unique);
     create table if not exists exercises (id integer primary key, exercise_type_id integer, workout_id integer, foreign key(exercise_type_id) references exercise_types(id), foreign key(workout_id) references workouts (id));
-    create table if not exists sets (id integer primary key, value real, reps integer, exercise_id integer, foreign key(exercise_id) references exercises (id));`
+    create table if not exists sets (id integer primary key, value real, reps integer, exercise_id integer, foreign key(exercise_id) references exercises (id));`,
+    `insert into exercise_types (exercise_name) values ('Bench Press');
+    insert into exercise_types (exercise_name) values ('Bicep Curls');
+    insert into exercise_types (exercise_name) values ('Lat Pulldowns');`
 ];
 
 async function execMigrations(startVersion: number){
@@ -37,7 +40,29 @@ export default {
             });
         });
     },
-    add: (workout: WorkoutProps) => console.log("add"),
+    getExerciseNames: () => {
+        db.transaction(tx => {
+            tx.executeSql("Select exercise_name from exercises", [],
+            async (_, { rows: { _array } }) => {
+                console.log(_array);
+            });
+        });
+    },
+    /*add: (workout: WorkoutProps) => {
+        db.transaction(tx => {
+            tx.executeSql("insert into workouts (date) values (?)", [workout.date],
+            async (_, { rows: { _array }}) => {
+                let workoutId = _array[0];
+                for(let exercise in workout.exercises){
+                    tx.executeSql("insert or ignore into exercise_types (exercise_name) values (?)", [exercise],
+                    async (_, { rows: { _array }}) => {
+                        let exerciseId = _array[0];
+                        tx.executeSql("insert into exercises (workout_id, exercise_type_id) values (?, ?)", [workoutId, exerciseId])
+                    })
+                }
+            });
+        })
+    },*/
     delete: (id: Number) => console.log("delete"),
     update: (workout: WorkoutProps) => console.log("update"),
     find: (id: Number) => console.log("find"),

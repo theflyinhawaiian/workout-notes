0af54eb157bcf8cb3b264d726a26df42aa5ecb92
Peter Mullins <jc310xc@gmail.com>
2025-07-26 20:56:14 -0400

    export workout data from device as json

--- app/settings.tsx
+++ app/settings.tsx
@@ -1,11 +1,27 @@
 import { Pressable, View, Text } from "react-native";
 import HorizontalDivider from "../components/HorizontalDivider";
+import workoutRepository from "../data/workoutRepository";
+import { useSQLiteContext } from "expo-sqlite";
+import * as FileSystem from 'expo-file-system';
+import { showSnackbar } from "../util/snackbars";
+import * as Sharing from 'expo-sharing';
+import { DateTime } from "luxon";
 
 export default function Settings () {
+    const db = useSQLiteContext();
 
-    const exportData = () => {
-        // Placeholder for export functionality
-        console.log("Exporting data...");
+    const exportData = async () => {
+        const workouts = await workoutRepository.getAll(db);
+
+        const dataString = JSON.stringify(workouts, null, 2);
+
+        const fileName = `workout_data_${DateTime.now().toFormat('yyyy-MM-dd')}.json`;
+        const fileUri = `${FileSystem.cacheDirectory}/${fileName}`;
+
+        await FileSystem.writeAsStringAsync(fileUri, dataString);
+
+        showSnackbar(`Data exported to ${fileUri}`);
+        await Sharing.shareAsync(fileUri);
     };
 
     return (
